6c4a40d184d2b3d6555d2877fe51ccbe16b80d9b
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-09-13 20:58:21 +0300

    base color constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,7 @@
 export const constants = {
-  
+  COLOR_BASE_DARK_BLUE = '#003d60',
+  COLOR_BASE_CYAN = '#00aab7',
+  COLOR_BASE_GREY = '#b8ccc6',
+  COLOR_BASE_LIGHT = '#f1ece9',
+  COLOR_BASE_SAND = '#f9e3be',
 }
