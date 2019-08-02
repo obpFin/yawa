74adcf3f536c00f66381638b51375c539566415d
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-08-02 10:57:43 +0300

    today screen skeleton

---
+++ lib/screens/today_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TodayScreen extends StatelessWidget {
+  static const pageRoute = 'today';
+  
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Today'),
+    );
+  }
+}
